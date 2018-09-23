30037b12c42454d6a73a4b6b2a6562c687f18950
Maleic Acid <kazukuro0824@gmail.com>
2018-09-23 18:57:45 +0900

    First commit

--- modeling/Class1.cs
+++ modeling/Class1.cs
@@ -2,8 +2,14 @@
 
 namespace modeling
 {
-    public class VModel
+    public class MotorModel
     {
-
+        public double Inductance { get; set; }
+        public double Resistance { get; set; }
+        public double K { get; set; }
+        public double Inertial_load { get; set; }
+        public double Voltage { get; set; }
+        public double PwmFreq_Hz { get; set; }
+        
     }
 }
