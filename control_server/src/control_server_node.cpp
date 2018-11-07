cf22b08dd6ad69b4d84a53f2b684097058896f37
maleicacid <kazukuro0824@gmail.com>
2018-11-07 19:35:36 +0900

    Scilabを叩いて制御に関する情報を返すサービスノードを作った 何故かscilabのリンクが通らない

---
+++ control_server/src/control_server_node.cpp
@@ -0,0 +1,50 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2018/11/07
+ *      Author: kazuki
+ */
+#include "ros/ros.h"
+#include "control_server/reference.h"
+#include "control_server/C1.h"
+
+#include "sci_bridge.h"
+
+
+bool Tuning(control_server::C1::Request &req, control_server::C1::Response &res)
+{
+  //res.sum = req.a + req.b;
+
+  return true;
+}
+
+bool getRef(control_server::reference::Request &req, control_server::reference::Response &res)
+{
+  //res.sum = req.a + req.b;
+
+  return true;
+}
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "control_server");
+	ros::NodeHandle n;
+
+	ROS_INFO("control_server: Initializing Scilab");
+
+	init_sci();
+	ROS_INFO("control_server: Success");
+
+
+	ros::ServiceServer C1srv = n.advertiseService("control_server_C1", Tuning);
+	ros::ServiceServer refsrv = n.advertiseService("control_server_reference", getRef);
+	ROS_INFO("control_server: Ready");
+
+
+	ros::spin();
+
+	return 0;
+}
+
+
