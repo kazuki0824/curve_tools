73810683ea6be686e2fd594cb8c8f2dfc00fc1db
Maleic Acid <kazukuro0824@gmail.com>
2018-11-14 02:03:57 +0900

    モータのトルク定数と慣性モーメントを追加

--- control_server/src/motor_config.h
+++ control_server/src/motor_config.h
@@ -1,7 +1,7 @@
 static const double E = 24;
 static const double b = 0;
-static const double J = 0.1; //TODO:
-static const double K = 0.1; //TODO:
+static const double J = 0.001020237876; //TODO:
+static const double K = 0.008; //TODO:
 static const double R = 0.485;
 static const double L = 72.3/(10^6);
 static const double T_pwm = 1/343;
