ca586a657b56df18071427a570c5121f5b018ed1
maleicacid <kazukuro0824@gmail.com>
2018-11-20 19:05:35 +0900

    base_controlを書いた

---
+++ base_control/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2018/11/20
+ *      Author: maleicacid
+ */
+
+#include <ros/ros.h>
+#include <iostream>
+#include <termios.h>
+
+using namespace std;
+
+
+int main()
+{
+	
+}
