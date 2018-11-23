f67b28febe7613d7353f05467cecaac926dc688b
Maleic Acid <kazukuro0824@gmail.com>
2018-11-23 17:06:34 +0900

    車輪を計算する小さな行列積をOpenMPで実装した

--- base_control/src/main.cpp
+++ base_control/src/main.cpp
@@ -15,11 +15,40 @@
 
 using namespace std;
 
+const double a = 308.432;
+const double b = 308.432;
+const double radius = 0 ;
 
-
+#define rows 4
+#define cols 1
 void refMsgCallback(const base_control::myTwistacc::ConstPtr accel)
 {
-    
+    //Matrix mul
+    double trans[rows][3] = {
+        {-1, 1, (a+b)},
+        {1 , 1, -(a+b)},
+        {-1, 1, -(a+b)},
+        {1,  1, (a+b)}
+    };
+    double twist_x[1][3] = {{0,0,0}};
+    double result[rows][cols];
+    #pragma omp parallel for
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            double cc = 0.0;
+            for (int k = 0; k < 3; k++) {
+                cc += trans[i][k] * twist_x[j][k];
+            }
+            result[i][j] = cc;
+        }
+    }
+    //Scilab ref
+
+    /* critical section start */
+
+    /* end */
 }
 
 int main(int argc, char** argv)
@@ -41,7 +70,7 @@ int main(int argc, char** argv)
     //ROS
 	ros::init(argc, argv, "base_control");
 	ros::NodeHandle nh("~");
-	ros::Subscriber s_twist = nh.subscribe("v_accel",100, refMsgCallback);
+	ros::Subscriber s_twist = nh.subscribe("v_accel", 100, refMsgCallback);
     while(true){
         /* code */
         ros::spinOnce();
