a84c567bd4fdde6528307b935e4f2e85ee829445
Maleic Acid <kazukuro0824@gmail.com>
2018-11-23 13:33:52 +0900

    Twistaccを追加

--- base_control/src/main.cpp
+++ base_control/src/main.cpp
@@ -7,10 +7,20 @@
 
 #include <ros/ros.h>
 #include <iostream>
+#include <thread>
 #include "comm.h"
+
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Accel.h>
+#include "base_control/myTwistacc.h"
 
 using namespace std;
 
+void refMsgCallback(const geometry_msgs::Pose::ConstPtr accel)
+{
+	//TODO:
+	glutPostRedisplay();
+}
 
 int main(int argc, char** argv)
 {
@@ -26,4 +36,15 @@ int main(int argc, char** argv)
         motors.push_back(a);
     }
 
+    //threads
+    
+    //ROS
+	ros::init(argc, argv, "base_control");
+	ros::NodeHandle nh("~");
+	ros::Subscriber s_twist = nh.subscribe("v_accel",100, refMsgCallback);
+    while(true){
+        /* code */
+        ros::spinOnce();
+    }
+    
 }
